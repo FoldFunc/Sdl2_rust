10b8a9621caad4ccc7885ae3fd5c132572ccc619
FoldFunc <fold.func@gmail.com>
2025-06-06 11:42:57 +0200

    trying to make an inpuot field

--- button/src/main.rs
+++ button/src/main.rs
@@ -1,58 +1,135 @@
-extern crate sdl2;
-
-use std::time::Duration;
-
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::libc::input_absinfo;
+use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
-fn main() {
-    let (sdl_content, _video_subsysstem, window, win_height, win_width) =
-        window_init("SDL2 Button".to_string(), 720, 720);
-    let canvas = window.into_canvas().build().unwrap();
-    let event_pump = sdl_content.event_pump().unwrap();
-    main_loop(canvas, event_pump, win_height, win_width)
+use std::time::Duration;
+struct Input_bar {
+    x: i32,
+    y: i32,
+    w: u32,
+    h: u32,
+    clicked: bool,
+    color: (u8, u8, u8),
+}
+struct Button {
+    x: i32,
+    y: i32,
+    w: u32,
+    h: u32,
+    color: (u8, u8, u8),
 }
 
-fn window_init(
-    window_name: String,
-    win_height: u32,
-    win_width: u32,
-) -> (sdl2::Sdl, sdl2::VideoSubsystem, Window, u32, u32) {
-    let sdl_content = sdl2::init().unwrap();
-    let video_subsysstem = sdl_content.video().unwrap();
-    let window = video_subsysstem
-        .window(&window_name, win_width, win_height)
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("SDL2 Button", 720, 720)
         .position_centered()
         .build()
         .unwrap();
-    (sdl_content, video_subsysstem, window, win_height, win_width)
-}
 
-fn main_loop(
-    mut canvas: Canvas<Window>,
-    mut event_pump: sdl2::EventPump,
-    win_height: u32,
-    win_width: u32,
-) {
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let button_register = Button {
+        x: 240,
+        y: 400,
+        w: 200,
+        h: 100,
+        color: (255, 0, 0),
+    };
+    let mut input_bar = Input_bar {
+        x: 240,
+        y: 300,
+        w: 200,
+        h: 50,
+        clicked: false,
+        color: (100, 0, 0),
+    };
+
     'running: loop {
         for event in event_pump.poll_iter() {
-            if let Event::Quit { .. }
-            | Event::KeyDown {
-                keycode: Some(Keycode::Escape),
-                ..
-            } = event
-            {
-                break 'running;
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+
+                Event::MouseButtonDown {
+                    x, y, mouse_btn, ..
+                } => {
+                    if mouse_btn == MouseButton::Left {
+                        if point_in_button(x, y, &button_register) {
+                            println!("Button clicked!");
+                        }
+                    }
+                    if mouse_btn == MouseButton::Right {
+                        if point_in_button(x, y, &button_register) {
+                            println!("Right mouse button clicked");
+                        }
+                    }
+                }
+                Event::MouseButtonUp {
+                    x, y, mouse_btn, ..
+                } => {
+                    if mouse_btn == MouseButton::Left {
+                        if point_in_input_bar(x, y, &input_bar) {
+                            input_bar.clicked = !input_bar.clicked;
+                            println!("Change to: {}", input_bar.clicked);
+                        }
+                    }
+                }
+
+                _ => {}
             }
         }
-        let _ = draw_window(&mut canvas, win_height, win_width);
+
+        canvas.set_draw_color(Color::RGB(100, 100, 100));
+        canvas.clear();
+        let _ = draw_shapes(&mut canvas, &button_register, &input_bar);
     }
 }
-fn draw_window(canvas: &mut Canvas<Window>, win_height: u32, win_width: u32) {
-    canvas.set_draw_color(Color::RGB(100, 100, 100));
-    canvas.clear();
+fn draw_shapes(canvas: &mut Canvas<Window>, button_register: &Button, input_field: &Input_bar) {
+    canvas.set_draw_color(Color::RGB(
+        button_register.color.0,
+        button_register.color.1,
+        button_register.color.2,
+    ));
+    let rect = Rect::new(
+        button_register.x,
+        button_register.y,
+        button_register.w,
+        button_register.h,
+    );
+    canvas.fill_rect(rect).unwrap();
+
+    canvas.set_draw_color(Color::RGB(
+        input_field.color.0,
+        input_field.color.1,
+        input_field.color.2,
+    ));
+    let input_f = Rect::new(input_field.x, input_field.y, input_field.w, input_field.h);
+    canvas.fill_rect(input_f).unwrap();
+
     canvas.present();
+
     std::thread::sleep(Duration::from_millis(16));
+}
+fn point_in_button(x: i32, y: i32, button: &Button) -> bool {
+    x >= button.x
+        && x <= button.x + button.w as i32
+        && y >= button.y
+        && y <= button.y + button.h as i32
+}
+fn point_in_input_bar(x: i32, y: i32, button: &Input_bar) -> bool {
+    x >= button.x
+        && x <= button.x + button.w as i32
+        && y >= button.y
+        && y <= button.y + button.h as i32
 }
